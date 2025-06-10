6b1a7801574b90c48bdf5ae55bea5d3f41ff8db7
Dah Sidi Abdallah <61889392+DahSidiAbdallah@users.noreply.github.com>
2025-06-10 11:47:52 +0000

    Updated Auth.tsx

--- src/components/ForgotPassword.tsx
+++ src/components/ForgotPassword.tsx
@@ -94,7 +94,7 @@ export function ForgotPassword() {
                 <div className="rounded-md bg-red-50 p-4 mb-6">
                   <div className="flex">
                     <div className="flex-shrink-0">
-                      <AlertTriangle className="h-5 w-5 text-red-400" aria-hidden="true" />
+                      <AlertTriangle className="h-5 w-5 text-red-400\" aria-hidden="true" />
                     </div>
                     <div className="ml-3">
                       <h3 className="text-sm font-medium text-red-800">{error}</h3>
